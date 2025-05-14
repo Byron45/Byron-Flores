09b8e022b2ad58f1712e92dcd6386232ca46d988
Byron Flores <168240410+Byron45@users.noreply.github.com>
2025-05-14 01:16:01 -0500

    Delete ProyectoAndroid directory

--- ProyectoAndroid/app/src/main/java/com/example/app_vehiculos/model/User.kt
+++
@@ -1,9 +0,0 @@
-package com.example.app_vehiculos.model
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "users")
-data class User(
-    @PrimaryKey val username: String
-)
